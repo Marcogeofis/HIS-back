33022c87308b88c2e699b0accbc3163f7938cd75
Marco Rivera <marcogeofis@gmail.com>
2022-11-09 12:17:54 -0600

    Se agrego la carpeta configuracion para conectar la base de datos con la libreria dotenv y un archivo env para usar variables de entorno y darle mayor seguridad a estas conexiones a la App

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,11 +1,24 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
+const { config } = require('../config/config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+console.log(URI)
+const pool = new Pool({ connectionString: URI });
+
+/**
+ *SUSTITUIMOS EL POOL DE ABAJO POR EL NUEVO POOL
+
+ const pool = new Pool({
     host: 'localhost',
     port: 5432,
     user: 'antri',
     password: 'bbstart',
     database: 'my_schoolbb'
   });
+
+ */
 
 module.exports = pool;
